948f2d4d0623e878821b776147db5d5e7acdfc97
chakra13 <chatrynguemen@gmail.com>
2025-03-27 21:45:23 +0100

    Good structure

--- models/note.js
+++ models/note.js
@@ -1,29 +1,10 @@
 const mongoose = require('mongoose')
 
-
-const url = process.env.MONGODB_URI
-
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
-  content:{
+  content: {
     type: String,
-    minLenght: 5,
-    required: true
-  },
-  date: {
-    type: Date,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -35,6 +16,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
